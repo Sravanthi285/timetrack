0f71b9098961c1d162134e0e51db2eae76ac6f18
sravanthi <sravanthikarnapu@gmail.com>
2026-02-11 09:47:59 +0530

    Debug: Add request logging

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const session = require('express-session');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Request Logger
+app.use((req, res, next) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+    next();
+});
+
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
